9dba417ed64e50f9042b29b088562469c059383f
linlin20ai <3357929671@qq.com>
2024-09-05 17:17:48 +0800

    项目初建

---
+++ ai/src/main/java/com/example/ai/AiApplication.java
@@ -0,0 +1,16 @@
+package com.example.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 林圣涛
+ */
+@SpringBootApplication
+public class AiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplication.class, args);
+    }
+
+}
