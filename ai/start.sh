99f222b7c92e8ae4937c106f4f0848e013a82b4f
linlin20ai <3357929671@qq.com>
2024-09-14 14:39:16 +0800

    key

--- ai/start.sh
+++ ai/start.sh
@@ -1,4 +1,4 @@
-docker run -p 8080:8080 --name ai-api -d shengtaolin/ai-api -e PARAMS="--sever.port=80 --chatgpt.host=https://sdk.xfg.im/ --chatgpt.apiKey=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 --wx.config.originalid=gh_c5ce6e4a0e0e  --wx.config.appid=wxad979c0307864a66 --wx.config.gatewayAddress=b8b6" -d fuzhengwei/chatgpt-api: 1.6
+docker run -p 8080:8080 --name ai-api -d shengtaolin/ai-api -e PARAMS="--sever.port=80 --chatgpt.host=https://sdk.xfg.im/ --chatgpt.apiKey= --wx.config.originalid=gh_c5ce6e4a0e0e  --wx.config.appid=wxad979c0307864a66 --wx.config.gatewayAddress=b8b6" -d fuzhengwei/chatgpt-api: 1.6
 
 
 
